e66b580d4027db220f0c0a5c3c3d1b92d487b781
wgondal <waleed.gondal@tuebingen.mpg.de>
2018-10-25 14:47:20 +0200

    Model specification for LSUN models is added

--- ganstab/gan_training/models/__init__.py
+++ ganstab/gan_training/models/__init__.py
@@ -1,13 +1,15 @@
 from ganstab.gan_training.models import (
-    resnet, resnet2, 
+    resnet, resnet2,resnet4, 
 )
 
 generator_dict = {
     'resnet': resnet.Generator,
     'resnet2': resnet2.Generator,
+    'resnet4': resnet4.Generator,
 }
 
 discriminator_dict = {
     'resnet': resnet.Discriminator,
     'resnet2': resnet2.Discriminator,
+    'resnet4' : resnet4.Discriminator,
 }
