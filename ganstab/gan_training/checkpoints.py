fa1dc7a1166460cda7d0d6a9ed23c6c873d2a6e0
Patsorn <patsorn.sangkloy@gmail.com>
2019-06-30 00:17:59 -0400

    map at load

--- ganstab/gan_training/checkpoints.py
+++ ganstab/gan_training/checkpoints.py
@@ -4,10 +4,9 @@ import warnings
 
 
 class CheckpointIO(object):
-    def __init__(self, checkpoint_dir='./chkpts',map_location='gpu', **kwargs):
+    def __init__(self, checkpoint_dir='./chkpts', **kwargs):
         self.module_dict = kwargs
         self.checkpoint_dir = checkpoint_dir
-        self.map_location = map_location
 
         if not os.path.exists(checkpoint_dir):
             os.makedirs(checkpoint_dir)
@@ -23,13 +22,13 @@ class CheckpointIO(object):
             outdict[k] = v.state_dict()
         torch.save(outdict, filename)
 
-    def load(self, filename):
+    def load(self, filename,map_location='gpu'):
         fpath = os.path.join(self.checkpoint_dir, filename)
 
         # print('??', self.checkpoint_dir)
         if os.path.exists(fpath):
             print('=> Loading checkpoint from {}'.format(fpath))
-            out_dict = torch.load(fpath,self.map_location)
+            out_dict = torch.load(fpath,map_location=map_location)
             it = out_dict['it']
             for k, v in self.module_dict.items():
                 if k in out_dict:
